484c5f4699ec473f6623d9abebe80a1dc99cc121
tkdanil <marvelsuper654@gmail.com>
2024-12-01 14:49:20 +0300

    Task 1

---
+++ main.py
@@ -0,0 +1,10 @@
+# project_1
+
+# Есть 5 документов в формате .docx. Напишите скрипт, который изменит во всех документах:
+# - шрифт – Times New Roman
+# - размер шрифта – 14
+# - межстрочный интервал – 1,5
+# Используйте библиотеку python-docx
+
+# Ссылка на файлы:
+# https://drive.google.com/drive/folders/1zGuYE9Decw6_iS7T3WKfdzncTE51ZvOP?usp=sharing 
