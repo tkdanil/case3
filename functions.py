3b1d7f9f6a0adcf6d8f883299f80146852bb5e76
tkdanil <marvelsuper654@gmail.com>
2024-12-21 00:04:12 +0300

    PULL

--- functions.py
+++ functions.py
@@ -8,4 +8,4 @@ for filename in os.listdir("C:\\Users\\Alex2\\кейс 3"):
     if filename.endswith('.docx'): 
         file_path = os.path.join("C:\\Users\\Alex2\\кейс 3", filename) 
         format_docx(file_path) 
-        print(f"Форматирование завершено для: {filename}")
+        print(f"Форматирование завершено для: {filename}")   
