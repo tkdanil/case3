73b39d26d45dcd153b925fa3b116b9f98b8e82db
tkdanil <marvelsuper654@gmail.com>
2024-12-21 00:32:59 +0300

    Доделал мэйн

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
+## main стартовый модуль проекта
 
+from smth_mod import multiply
+
+def main():
+    # TODO - сделай вызов функций из functions
+    multiply([1, 2, 3])
+
+# инициализационный скрипт
+if __name__ == "__main__":
+    main()
