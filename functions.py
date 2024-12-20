7601cecccdb3d4a170df62fdf08d66f74caacf7d
tkdanil <marvelsuper654@gmail.com>
2024-12-21 00:35:22 +0300

    доделал functions

--- functions.py
+++ functions.py
@@ -33,11 +33,11 @@ def format_docx(file_path):
 # functions модуль дляимпорта функций для решения задачи
    # Сохраняем изменения 
    doc.save(file_path) 
-# Путь к папке с документами 
-folder_path = '"C:\\Users\\Alex2\\кейс 3"'  # Замените на путь к вашей папке
-# Проходим по всем файлам в папке 
-for filename in os.listdir("C:\\Users\\Alex2\\кейс 3"): 
-    if filename.endswith('.docx'): 
-        file_path = os.path.join("C:\\Users\\Alex2\\кейс 3", filename) 
-        format_docx(file_path) 
-        print(f"Форматирование завершено для: {filename}")
+
+def change_format(folder_path):
+    # Проходим по всем файлам в папке
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.docx'):
+            file_path = os.path.join(folder_path, filename)
+            format_docx(file_path)
+            print(f"Форматирование завершено для: {filename}")
