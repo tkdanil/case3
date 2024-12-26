6dc2e870efcaea335fdf7b568392d5baefe426f3
tkdanil <marvelsuper654@gmail.com>
2024-12-26 13:40:17 +0300

    Переделал main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-
 ## main стартовый модуль проекта
 
-from smth_mod import multiply
-
-def main():
-    # TODO - сделай вызов функций из functions
-    multiply([1, 2, 3])
-
-# инициализационный скрипт
-if __name__ == "__main__":
+from functions import format_docx, change_format 
+import os 
+ 
+def main(): 
+    # Вызов функции change_format() 
+    change_format ("C:\\Users\\Alex2\\кейс 3") 
+ 
+def change_format(folder_path): 
+    # Проход по всем файлам в папке 
+    for filename in os.listdir("C:\\Users\\Alex2\\кейс 3"):  
+        if filename.endswith('.docx'):  
+            file_path = os.path.join("C:\\Users\\Alex2\\кейс 3", filename)  
+            format_docx(file_path)  
+            print(f"Форматирование завершено для: {filename}") 
+ 
+# Инициализация скрипта 
+if __name__- "__main__": 
     main()
